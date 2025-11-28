64634cba6d4e4c20e24889da8a9187129786e2cb
Igor Henrique <ihenrique@sp.senai.br>
2025-11-28 11:18:31 -0300

    test: create test for login

--- src/main/java/henrique/igor/restaurantmanagementapi/errors/ApiExceptionHandler.java
+++ src/main/java/henrique/igor/restaurantmanagementapi/errors/ApiExceptionHandler.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.*;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -20,23 +21,6 @@ import java.util.Map;
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     private final MessageSource messageSource;
-
-
-    @ExceptionHandler(BusinessRuleException.class)
-    public ResponseEntity<Object> handlerBusinessRuleException(BusinessRuleException exception, WebRequest request) {
-        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
-
-        if (exception.getCode() == ExceptionCode.DUPLICATED_RESOURCE) {
-            status = HttpStatus.CONFLICT;
-        } else if (exception.getCode() == ExceptionCode.ENTITY_NOT_FOUND) {
-            status = HttpStatus.NOT_FOUND;
-        }
-
-        ErrorResponse body = buildErrorResponse(status, exception.getCode(), exception.getDetails());
-
-        log.error("Business Error: [Code: {}] - [Details: {}]", body.getCode(), body.getDetails());
-        return handleExceptionInternal(exception, body, new HttpHeaders(), status, request);
-    }
 
     private ErrorResponse buildErrorResponse(HttpStatus status, ExceptionCode exceptionCode, Object details) {
         String message;
@@ -72,6 +56,40 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         });
 
         return translatedMap;
+    }
+
+    @ExceptionHandler(BusinessRuleException.class)
+    public ResponseEntity<Object> handlerBusinessRuleException(BusinessRuleException exception, WebRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+
+        if (exception.getCode() == ExceptionCode.DUPLICATED_RESOURCE) {
+            status = HttpStatus.CONFLICT;
+        } else if (exception.getCode() == ExceptionCode.ENTITY_NOT_FOUND) {
+            status = HttpStatus.NOT_FOUND;
+        }
+
+        ErrorResponse body = buildErrorResponse(
+                status,
+                exception.getCode(),
+                exception.getDetails()
+        );
+
+        log.error("Business Error: [Code: {}] - [Details: {}]", body.getCode(), body.getDetails());
+        return handleExceptionInternal(exception, body, new HttpHeaders(), status, request);
+    }
+
+    @ExceptionHandler(value = { BadCredentialsException.class })
+    public ResponseEntity<Object> badCredentialsExceptionHandler(Exception exception, WebRequest request) {
+        HttpStatus status = HttpStatus.UNAUTHORIZED;
+
+        ErrorResponse body = buildErrorResponse(
+                status,
+                ExceptionCode.BAD_CREDENTIALS,
+                exception.getMessage()
+        );
+
+        log.error("Authentication Error: {}", exception.getMessage(), exception);
+        return handleExceptionInternal(exception, body, new HttpHeaders(), HttpStatus.UNAUTHORIZED, request);
     }
 }
 
