304f7472189d520f27d3a4d9d5bd40f208a87cd2
Igor Henrique <ihenrique@sp.senai.br>
2025-12-02 17:15:23 -0300

    feat: create Dish entity and migration

---
+++ src/main/java/henrique/igor/restaurantmanagementapi/entities/Dish.java
@@ -0,0 +1,42 @@
+package henrique.igor.restaurantmanagementapi.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "dishes")
+public class Dish {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID dishId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    private String description;
+
+    //TODO - add images
+
+    @CreatedDate
+    @DateTimeFormat(pattern = "YYYY-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @DateTimeFormat(pattern = "YYYY-MM-dd HH:mm:ss")
+    private LocalDateTime updatedAt;
+}
