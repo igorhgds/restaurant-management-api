04e970ec5c32346962d6501609ae18d39e39e3a2
Igor Henrique <ihenrique@sp.senai.br>
2025-12-20 15:19:21 -0300

    feat: add category for dishes

--- src/main/java/henrique/igor/restaurantmanagementapi/entities/dtos/dish/request/CreateDishRequestDTO.java
+++ src/main/java/henrique/igor/restaurantmanagementapi/entities/dtos/dish/request/CreateDishRequestDTO.java
@@ -1,5 +1,6 @@
 package henrique.igor.restaurantmanagementapi.entities.dtos.dish.request;
 
+import henrique.igor.restaurantmanagementapi.enums.Category;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -8,6 +9,7 @@ import java.math.BigDecimal;
 public record CreateDishRequestDTO(
         @NotBlank String name,
         @NotNull BigDecimal price,
+        @NotNull Category category,
         String description
 ) {
 }
--- src/main/resources/db/migration/V2__create_dishes_table.sql
+++ src/main/resources/db/migration/V2__create_dishes_table.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS dishes(
     dish_id     UUID                    PRIMARY KEY,
     name        VARCHAR(150)            UNIQUE  NOT NULL,
     price       DECIMAL(10, 2)          NOT NULL,
+    category    VARCHAR(50)             NOT NULL,
     description TEXT,
     created_at  TIMESTAMP               NOT NULL,
     updated_at  TIMESTAMP
